4ebda02485c0d3a279cdd3e3a40b5c2b1a4b3add
Tri Kusumawardana <trikusumawardana6@gmail.com>
2022-12-16 09:34:04 +0700

    edit studentInfo

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
 const url = "YOURDEPLOYURL" // TODO: replace this
 const studentName = "Tri Kusumawardana" // TODO: replace this
-const studentId = "FE268" // TODO: replace this
+const studentId = "FE2683205" // TODO: replace this
 
 export { url, studentName, studentId }
